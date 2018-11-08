c37191e197e3790063a8786e1d75ba7b6831eb30
Lucas Clay <38572212+mlclay@users.noreply.github.com>
2018-11-08 11:44:15 -0500

    LoadVault container names are now statically defined

--- src/shipchain/LoadVault.ts
+++ src/shipchain/LoadVault.ts
@@ -18,47 +18,52 @@ import { Vault } from '../vaults/Vault';
 import { Wallet } from '../entity/Wallet';
 
 export class LoadVault extends Vault {
+
+    private static readonly TRACKING: string = 'tracking';
+    private static readonly SHIPMENT: string = 'shipment';
+    private static readonly DOCUMENTS: string = 'documents';
+
     constructor(storage_driver, id?) {
         super(storage_driver, id);
     }
 
     async initializeMetadata(author: Wallet) {
         await super.initializeMetadata(author);
-        this.getOrCreateContainer(author, 'tracking', 'embedded_list');
-        this.getOrCreateContainer(author, 'shipment', 'embedded_file');
-        this.getOrCreateContainer(author, 'documents', 'external_file_multi');
+        this.getOrCreateContainer(author, LoadVault.TRACKING, 'embedded_list');
+        this.getOrCreateContainer(author, LoadVault.SHIPMENT, 'embedded_file');
+        this.getOrCreateContainer(author, LoadVault.DOCUMENTS, 'external_file_multi');
         return this.meta;
     }
 
     async addTrackingData(author: Wallet, payload) {
-        await this.containers.tracking.append(author, payload);
+        await this.containers[LoadVault.TRACKING].append(author, payload);
     }
 
     async getTrackingData(author: Wallet) {
         await this.loadMetadata();
-        return await this.containers.tracking.decryptContents(author);
+        return await this.containers[LoadVault.TRACKING].decryptContents(author);
     }
 
     async addShipmentData(author: Wallet, shipment) {
-        await this.containers.shipment.setContents(author, JSON.stringify(shipment));
+        await this.containers[LoadVault.SHIPMENT].setContents(author, JSON.stringify(shipment));
     }
 
     async getShipmentData(author: Wallet) {
         await this.loadMetadata();
-        const contents = await this.containers.shipment.decryptContents(author);
+        const contents = await this.containers[LoadVault.SHIPMENT].decryptContents(author);
         return JSON.parse(contents);
     }
 
     async addDocument(author: Wallet, name: string, document: any) {
-        await this.containers.documents.setSingleContent(author, name, document);
+        await this.containers[LoadVault.DOCUMENTS].setSingleContent(author, name, document);
     }
 
     async getDocument(author: Wallet, name: string) {
         await this.loadMetadata();
-        return await this.containers.documents.decryptContents(author, name);
+        return await this.containers[LoadVault.DOCUMENTS].decryptContents(author, name);
     }
 
     async listDocuments() {
-        return await this.containers.documents.listFiles();
+        return await this.containers[LoadVault.DOCUMENTS].listFiles();
     }
 }
