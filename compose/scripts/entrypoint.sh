9af7cb16ed62e25eef3136eb25ebcc5af38b634f
Lucas Clay <38572212+mlclay@users.noreply.github.com>
2019-01-24 10:35:40 -0500

    Use wait-for-it.sh in entrypoint instead of Healthcheck in compose files
    Healthcheck has been deprecated in newer versions of Docker Compose

--- compose/scripts/entrypoint.sh
+++ compose/scripts/entrypoint.sh
@@ -3,14 +3,23 @@
 if [[ "$ENV" = "PROD" ]] || [[ "$ENV" = "DEMO" ]] || [[ "$ENV" = "STAGE" ]] || [[ "$ENV" = "DEV" ]];
 then
   /download-certs.sh
+  yarn run migrate
+
+else
+    echo "Waiting for dependencies to come up in the stack"
+    /wait-for-it.sh ${REDIS_NAME:-redis_db}:6379
+    /wait-for-it.sh ${PSQL_NAME:-psql}:5432
+    /wait-for-it.sh ${GETH_NAME:-geth-poa}:8545
+
+    # Run migrations
+    if [[ -z "$IS_DDO" ]];
+    then
+        yarn run migrate
+    else
+        echo "Skipping migrations"
+    fi
+
 fi
 
-# Run migrations
-if [[ -z "$IS_DDO" ]];
-then
-    yarn run migrate
-else
-    echo "Skipping migrations"
-fi
 
 exec "$@"
