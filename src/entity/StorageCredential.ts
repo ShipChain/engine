f5de9439bd3c1845660c01c792eef6a461aa3722
jianwel <ljw725@gmail.com>
2019-06-07 10:09:20 -0400

    applied the DRY patch by Lucas

--- src/entity/StorageCredential.ts
+++ src/entity/StorageCredential.ts
@@ -45,10 +45,8 @@ export class StorageCredential extends BaseEntity {
         credentials.title = attrs.title;
         credentials.driver_type = attrs.driver_type;
         credentials.base_path = attrs.base_path || './';
+        await credentials.setOptionsField(attrs.options);
 
-        const optionString: string = JSON.stringify(attrs.options || {});
-        const encryptString = await EncryptorContainer.defaultEncryptor.encrypt(optionString);
-        credentials.options = { EncryptedJson: encryptString };
         logger.debug(`Creating ${attrs.driver_type} StorageDriver ${attrs.title}`);
         return credentials;
     }
@@ -97,24 +95,32 @@ export class StorageCredential extends BaseEntity {
         return (await StorageCredential.getById(id)).getDriverOptions();
     }
 
+    private async setOptionsField(options: any) {
+        const optionString: string = JSON.stringify(options || {});
+        const encryptString = await EncryptorContainer.defaultEncryptor.encrypt(optionString);
+        this.options = { EncryptedJson: encryptString };
+    }
+
+    private async getOptionsField() {
+        const encryptString = this.options['EncryptedJson'];
+        const decryptedOptionString = await EncryptorContainer.defaultEncryptor.decrypt(encryptString);
+        return JSON.parse(decryptedOptionString);
+    }
+
     async update(title?: string, options?: any) {
         if (title) {
             this.title = title;
         }
 
         if (options) {
-            const optionString: string = JSON.stringify(options || {});
-            const encryptString = await EncryptorContainer.defaultEncryptor.encrypt(optionString);
-            this.options = { EncryptedJson: encryptString };
+            await this.setOptionsField(options);
         }
 
         await this.save();
     }
 
     async getDriverOptions() {
-        const encryptString = this.options['EncryptedJson'];
-        const decryptedOptionString = await EncryptorContainer.defaultEncryptor.decrypt(encryptString);
-        const decryptedOptions = JSON.parse(decryptedOptionString);
+        const decryptedOptions = await this.getOptionsField();
         return {
             ...decryptedOptions,
             driver_type: this.driver_type,
