937d0cd8fb507efde94694bd597bcd51f88510ff
Lucas Clay <38572212+mlclay@users.noreply.github.com>
2019-07-08 13:40:04 -0400

    RPC Vault unit tests for Sequence retrieval

--- rpc/__tests__/vault.ts
+++ rpc/__tests__/vault.ts
@@ -1836,6 +1836,63 @@ export const RPCVaultTests = async function() {
                 fail(`Should not have thrown [${err}]`);
             }
         }));
+
+        it(`Throws when sequence is not for container`, mochaAsync(async () => {
+            try {
+                resetDate(); // <-- Very important!
+                const result: any = await CallRPCMethod(RPCVault.GetHistoricalShipmentData, {
+                    storageCredentials: localStorage.id,
+                    vaultWallet: fullWallet1.id,
+                    vault: testableLocalVaultId,
+                    sequence: 1,
+                });
+                fail("Did not Throw"); return;
+            } catch (err) {
+                expect(err.message).toEqual(`Ledger data at index 1 is not for container shipment`);
+            }
+        }));
+
+        it(`Returns intermediate data at a sequence for shipment data`, mochaAsync(async () => {
+            try {
+                resetDate(); // <-- Very important!
+                const result: any = await CallRPCMethod(RPCVault.GetHistoricalShipmentData, {
+                    storageCredentials: localStorage.id,
+                    vaultWallet: fullWallet1.id,
+                    vault: testableLocalVaultId,
+                    sequence: 2,
+                });
+                expect(result.success).toBeTruthy();
+                expect(result.historical_data).toEqual({
+                    shipment: {
+                        id: knownShipmentSchemaId,
+                        version: '0.0.1',
+                    },
+                });
+            } catch (err) {
+                fail(`Should not have thrown [${err}]`);
+            }
+        }));
+
+        it(`Returns other data at a sequence for shipment data`, mochaAsync(async () => {
+            try {
+                resetDate(); // <-- Very important!
+                const result: any = await CallRPCMethod(RPCVault.GetHistoricalShipmentData, {
+                    storageCredentials: localStorage.id,
+                    vaultWallet: fullWallet1.id,
+                    vault: testableLocalVaultId,
+                    sequence: 6,
+                });
+                expect(result.success).toBeTruthy();
+                expect(result.historical_data).toEqual({
+                    shipment: {
+                        id: knownShipmentSchemaId,
+                        version: '0.0.2',
+                    },
+                });
+            } catch (err) {
+                fail(`Should not have thrown [${err}]`);
+            }
+        }));
     });
 
     describe('GetHistoricalTrackingData', function() {
@@ -2063,6 +2120,63 @@ export const RPCVaultTests = async function() {
                         some: 'data',
                     },{
                         more: 'data'
+                    }],
+                });
+            } catch (err) {
+                fail(`Should not have thrown [${err}]`);
+            }
+        }));
+
+        it(`Throws when no data exists for sequence`, mochaAsync(async () => {
+            try {
+                resetDate(); // <-- Very important!
+                const result: any = await CallRPCMethod(RPCVault.GetHistoricalTrackingData, {
+                    storageCredentials: localStorage.id,
+                    vaultWallet: fullWallet1.id,
+                    vault: testableLocalVaultId,
+                    sequence: 0,
+                });
+                fail("Did not Throw"); return;
+            } catch (err) {
+                expect(err.message).toEqual(`No data found at specific sequence`);
+            }
+        }));
+
+        it(`Returns Tracking data up to a sequence 2`, mochaAsync(async () => {
+            try {
+                resetDate(); // <-- Very important!
+                const result: any = await CallRPCMethod(RPCVault.GetHistoricalTrackingData, {
+                    storageCredentials: localStorage.id,
+                    vaultWallet: fullWallet1.id,
+                    vault: testableLocalVaultId,
+                    sequence: 3,
+                });
+                expect(result.success).toBeTruthy();
+                expect(result.historical_data).toEqual({
+                    tracking: [{
+                        'some': 'data',
+                    }],
+                });
+            } catch (err) {
+                fail(`Should not have thrown [${err}]`);
+            }
+        }));
+
+        it(`Returns Tracking data up to a sequence 3`, mochaAsync(async () => {
+            try {
+                resetDate(); // <-- Very important!
+                const result: any = await CallRPCMethod(RPCVault.GetHistoricalTrackingData, {
+                    storageCredentials: localStorage.id,
+                    vaultWallet: fullWallet1.id,
+                    vault: testableLocalVaultId,
+                    sequence: 8,
+                });
+                expect(result.success).toBeTruthy();
+                expect(result.historical_data).toEqual({
+                    tracking: [{
+                        'some': 'data',
+                    },{
+                        'more': 'data',
                     }],
                 });
             } catch (err) {
@@ -2356,6 +2470,59 @@ export const RPCVaultTests = async function() {
                 fail("Did not Throw"); return;
             } catch (err) {
                 expect(err.message).toEqual(`No data found for date`);
+            }
+        }));
+
+        it(`Throws when sequence is not for container`, mochaAsync(async () => {
+            try {
+                resetDate(); // <-- Very important!
+                const result: any = await CallRPCMethod(RPCVault.GetHistoricalDocument, {
+                    storageCredentials: localStorage.id,
+                    vaultWallet: fullWallet1.id,
+                    vault: testableLocalVaultId,
+                    sequence: 1,
+                    documentName: 'none.png',
+                });
+                fail("Did not Throw"); return;
+            } catch (err) {
+                expect(err.message).toEqual(`Ledger data at index 1 is not for container documents`);
+            }
+        }));
+
+        it(`Throws when no data for specific document exists for sequence`, mochaAsync(async () => {
+            try {
+                resetDate(); // <-- Very important!
+                const result: any = await CallRPCMethod(RPCVault.GetHistoricalDocument, {
+                    storageCredentials: localStorage.id,
+                    vaultWallet: fullWallet1.id,
+                    vault: testableLocalVaultId,
+                    sequence: 3,
+                    documentName: 'none.png',
+                });
+                fail("Did not Throw"); return;
+            } catch (err) {
+                expect(err.message).toEqual(`No data found at specific sequence`);
+            }
+        }));
+
+        it(`Returns data at a sequence for specific document when exists`, mochaAsync(async () => {
+            try {
+                resetDate(); // <-- Very important!
+                const result: any = await CallRPCMethod(RPCVault.GetHistoricalDocument, {
+                    storageCredentials: localStorage.id,
+                    vaultWallet: fullWallet1.id,
+                    vault: testableLocalVaultId,
+                    sequence: 3,
+                    documentName: 'test.png',
+                });
+                expect(result.success).toBeTruthy();
+                expect(result.historical_data).toEqual({
+                    documents: {
+                        'test.png': knownDocumentContent,
+                    },
+                });
+            } catch (err) {
+                fail(`Should not have thrown [${err}]`);
             }
         }));
     });
